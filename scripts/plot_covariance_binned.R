cc6e1f60139f734c09b221b71bc94016dd617fe6
Austin Szatrowski <austinszatrowski@gmail.com>
2026-01-30 11:11:51 -0600

    binning looks good now

--- scripts/plot_covariance_binned.R
+++ scripts/plot_covariance_binned.R
@@ -1,34 +1,20 @@
 library(ggplot2)
+library(dplyr)
+x_axis_cuts_relhat <- seq(-1,1,0.005)
+cov_df <- readr::read_csv(snakemake@input$covariances_csv, show_col_types = FALSE) |>
+  dplyr::group_by(GCC = cut(Genetic_Covariance, x_axis_cuts_relhat)) |>
+  dplyr::mutate(n=n()) |>
+  dplyr::summarize(across(-contains("id"), function(x) mean(x, na.rm=TRUE)))
 
-cov_df <- readr::read_csv(snakemake@input$covariances_csv) |>
-  dplyr::mutate(
-    gcov_bin = cut(
-      Genetic_Covariance,
-      # breaks = seq(min(Genetic_Covariance), max(Genetic_Covariance), by = 0.05),
-      breaks = c(-Inf, seq(-0.25, 0.25, by = 0.05), 0.5, 0.75, Inf),
-      include.lowest = TRUE,
-      ordered_result = TRUE
-    )
-  ) |>
-  dplyr::group_by(gcov_bin) |>
-  dplyr::summarize(
-    mean_gcov = mean(Genetic_Covariance),
-    mean_y = mean(Y),
-    .groups = "drop"
-  )
-
-p <- ggplot(cov_df, aes(x = gcov_bin, y = mean_y)) +
+p <- ggplot(cov_df, aes(x = Genetic_Covariance, y = Y)) +
   geom_point() +
-  geom_line(group = 1) +
   labs(
     title = "Distribution of Y by Genetic Covariance Bins",
     subtitle = paste("Arch:", snakemake@wildcards$arch),
     x = "Genetic Covariance",
     y = "Phenotypic Covariance"
   ) +
-  theme(
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  ) 
+  theme_classic()
 
 ggsave(
   filename = snakemake@output$covariance_plot,
---
+++ scripts/sim_vct.py
@@ -0,0 +1,148 @@
+import xftsim as xft
+from xftsim.reproduce import RecombinationMap
+from xftsim.sim import Simulation # import as own object
+
+import numpy as np
+import random
+
+from minimal_simulation_utils import make_covariance_matrix, build_minimal_founders
+
+random.seed(1)
+founders, recomb = build_minimal_founders(
+    n_indivs=int(snakemake.params['n_indivs']),
+    m_variants=int(snakemake.params['m_variants']),
+    min_af=0.1,
+    max_af=0.5,
+    chrom=1
+)
+
+def additive_effects_freq(founder_haplotypes, n_causal = None, w = 1, h2 = 0):
+    """
+    Minimal additive effects builder based on allele frequencies.
+    """
+    # Extract per-variant allele frequencies and minor allele frequencies.
+    m =  founder_haplotypes.xft.m
+    allele_freqs = founder_haplotypes.af
+    m_allele_freqs = np.minimum(allele_freqs, 1-allele_freqs)[::2]
+    # choose n_causal sites to have an effect and the rest to be zero
+    beta = np.zeros((m,1))
+    print(f"Number of causal sites: {n_causal}.")
+
+    if n_causal is None or n_causal >= m:
+        raise ValueError("n_causal must be set and less than m")
+
+    # Apply thinning if n_causal is much smaller than m - just to speed up.
+    if n_causal < m // 10:
+        thinning_fraction = 0.2 
+        thin_m = int(thinning_fraction * m)
+        subset = np.random.choice(np.arange(m), thin_m, replace=False)
+        subset_maf = m_allele_freqs[subset]  
+        weights = (1 - subset_maf) ** w
+        p = weights / weights.sum()
+        causal_sites = np.random.choice(subset, n_causal, replace=False, p=p)
+    else:
+        weights = (1 - subset_maf) ** w
+        p = weights / weights.sum()
+        causal_sites = np.random.choice(np.arange(m), n_causal, replace=False, p=p)
+
+    # Assign effect sizes to the chosen causal sites.
+    beta[causal_sites] = np.random.randn(n_causal,1) * np.sqrt(h2)
+
+    print(f"Mean allele frequency: {np.mean(m_allele_freqs)}.")
+    print(f"Mean allele frequency at causal sites: {np.mean(m_allele_freqs[causal_sites])}.")
+
+    # Package effects in xftsim structures.
+    vindex = founder_haplotypes.xft.get_variant_indexer()
+    cindex = xft.index.ComponentIndex.from_product('Y', 'additiveGenetic')
+    effects = xft.effect.AdditiveEffects(beta=beta,
+                                         variant_indexer=vindex,
+                                         component_indexer=cindex)
+    
+    print(f"Done deciding effect sizes.")
+    return([effects,causal_sites])
+
+def vct_architecture(founder_haplotypes, h2, b2, parental_coeff = 1/2, n=1000, n_causal = None,w=0):
+    """
+    Function to set up VCT architecture. source: Marida, entirely.
+    """
+    # sets a dominance coefficient. b2m = coeff * b2m, b2m + b2p = b2
+    b2p = parental_coeff * b2
+    b2m = b2 - b2p
+    if parental_coeff!=1/2:
+        print(f"Paternal variance is {b2p} and maternal variance is {b2m}.")
+
+    # Make genetic component.
+    effects,causal_sites = additive_effects_freq(founder_haplotypes, n_causal = n_causal,h2 = 0.5, w=w)
+    print("Now making architecture...")
+    a_comp = xft.arch.AdditiveGeneticComponent(beta=effects,  component_name="additiveGenetic")
+    # making the vertical component
+    vert_input = xft.index.ComponentIndex.from_product(['HeritableFactor'], ['phenotype'], [0,1])
+    vert_input.comp_type ='intermediate'
+    vert_output = xft.index.ComponentIndex.from_product(['HeritableFactor'], ['vertical'], [-1])
+    # one part will be from parents
+    vt_comp = xft.arch.LinearVerticalComponent(input_cindex=vert_input,
+                                              output_cindex=vert_output,
+                                              founder_variances=[b2,b2],
+                                              coefficient_matrix=np.array([[1-parental_coeff, parental_coeff]]),
+                                              normalize = False)
+    # also has variance
+    vt_noise = xft.arch.AdditiveNoiseComponent(variances=[b2/2 * (1 + 1/n)], # extra factor to account for shrinking variance due to relatedness in sample
+                                               phenotype_name=['HeritableFactor'],
+                                               component_name='additiveNoise')
+    vind = xft.index.ComponentIndex.from_product(['HeritableFactor'],
+                                                 ['additiveNoise','vertical'])
+    # makes an intermediate phenotype for the vertical component
+    vtrans = xft.arch.SumAllTransformation(input_cindex=vind,output_component_name='phenotype',output_comp_type = 'intermediate')
+    # additive (non heritable) noise
+    a_noise = xft.arch.AdditiveNoiseComponent(variances=[1-h2-b2], phenotype_name=['Y'],component_name='additiveNoise')
+    # constructing the final phenotype
+    input_ind = xft.index.ComponentIndex(['HeritableFactor'], ['phenotype'],
+                                         comp_type='intermediate')
+    output_ind = xft.index.ComponentIndex(['Y'], ['heritableNoise'])
+    # feed heritable factor into the phenotype stack
+    coefficient_matrix = np.array([[1]])
+    causal_comp = xft.arch.LinearTransformationComponent(input_ind, output_ind,
+                                                   coefficient_matrix,normalize=False)
+    input_cindex = xft.index.ComponentIndex(phenotype_name = ['Y', 'Y','Y'],
+                                            component_name = ['heritableNoise',
+                                                              'additiveNoise',
+                                                              'additiveGenetic'])
+    strans = xft.arch.SumAllTransformation(input_cindex,output_component_name='phenotype',
+                                           output_comp_type = 'outcome')
+    arch = xft.arch.Architecture([vt_noise,
+                                    vt_comp,
+                                    vtrans,
+                                    a_noise,
+                                    a_comp,
+                                    causal_comp,
+                                    strans])
+    print("Done making architecture.")
+    return([arch,causal_sites])
+
+mating_regime = xft.mate.RandomMatingRegime(
+    mates_per_female=1,
+    offspring_per_pair=2,
+    female_offspring_per_pair="balanced",
+    exhaustive=True,
+)
+
+post_processors = [xft.proc.LimitMemory(n_haplotype_generations=1)]
+sim = xft.sim.Simulation(
+    founder_haplotypes=founders,
+    architecture=arch,
+    recombination_map=recomb,
+    mating_regime=mating_regime,
+    statistics=[],
+    post_processors=post_processors
+)
+
+print('Running generations...')
+sim.run(10)
+print('Complete.')
+    
+print('Building covariance matrix...')
+cov = make_covariance_matrix(sim, maf = 0.01, include_pedigree=False)
+print('Complete.')
+print('Writing covariance matrix...')
+cov.to_csv(snakemake.output['covariances_csv'], index=False)
+print('Complete.')
